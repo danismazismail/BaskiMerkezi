d51c6fec1b8d0d0043816d88c36d5fa8d0fd6b92
Ismail DANISMAZ <danismazismail@gmail.com>
2024-06-27 22:54:40 +0300

    Add project files.

---
+++ UsKirtasiye/Controllers/BuyController.cs
@@ -0,0 +1,193 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using UsKirtasiye.DB;
+using UsKirtasiye.Models;
+using UsKirtasiye.Shared;
+
+namespace UsKirtasiye.Controllers
+{
+    public class BuyController : Controller
+    {
+        private UsKirtasiyeEntities context;
+
+        public BuyController()
+        {
+            context = new UsKirtasiyeEntities();
+        }
+
+        [HttpGet]
+        public ActionResult Buy()
+        {
+            var currentuser = (UsKirtasiye.DB.Members)Session["LogonUser"];
+            if (Session["LogonUser"] == null || Session["Basket"] == null)
+            {
+                return RedirectToAction("Index", "Index");
+            }
+            List<Models.Product_Detail.ProductBasketModels> model = (List<Models.Product_Detail.ProductBasketModels>)Session["Basket"];
+            if (model == null)
+            {
+                model = new List<Models.Product_Detail.ProductBasketModels>();
+            }
+            TempData["TotalPrice"] = model.Select(x => x.Products.Price * x.Count).Sum();
+            int id = currentuser.Id;
+            ViewBag.adresler = context.Addresses.Where(x => x.Member_Id == id && x.IsActive == true).Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Buy(string Address)
+        {
+            var currentuser = (UsKirtasiye.DB.Members)Session["LogonUser"];
+            if (Session["LogonUser"] == null)
+            {
+                return RedirectToAction("Index", "Index");
+            }
+            else if (Address == null)
+            {
+                TempData["MyError"] = "Lutfen profilinizden adres bilgilerinizi giriniz";
+            }
+            else
+            {
+                try
+                {
+                    var basket = (List<Models.Product_Detail.ProductBasketModels>)Session["Basket"];
+                    var adresçevirme = Int64.Parse(Address);
+                    var adress = context.Addresses.FirstOrDefault(x => x.Id == adresçevirme);
+                    // Ödeme bildilrimi yapıldı
+                    // Spariş verildi
+                    // Ödeme Onaylandı
+                    var order = new DB.Orders()
+                    {
+                        AddedDate = DateTime.Now,
+                        Address = adress.AdresDescription,
+                        Member_Id = currentuser.Id,
+                        Id = Guid.NewGuid(),
+                        Status = "SV",
+                    };
+                    foreach (Models.Product_Detail.ProductBasketModels item in basket)
+                    {
+                        var orderdetail = new DB.OrderDetails();
+                        orderdetail.AddedDate = DateTime.Now;
+                        orderdetail.Price = item.Products.Price * item.Count;
+                        orderdetail.Product_Id = item.Products.Id;
+                        orderdetail.Quantity = item.Count;
+                        orderdetail.Id = Guid.NewGuid();
+                        order.OrderDetails.Add(orderdetail);
+                        var product = context.Products.FirstOrDefault(x => x.Id == item.Products.Id);
+                        if (product != null && product.UnitsInStock >= item.Count)
+                        {
+                            product.UnitsInStock = product.UnitsInStock - item.Count;
+                        }
+                        else
+                        {
+                            throw new Exception(string.Format("{0} ürünü için yeterli stok yoktur veya silinmiş bir ürünü almaya çalışıyorsunuz", item.Products.Name));
+                        }
+                    }
+                    context.Orders.Add(order);
+                    context.SaveChanges();
+                    await SendMail(currentuser, basket, order);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.MyError = ex.Message;
+                    throw;
+                }
+            }
+
+            return RedirectToAction("Buy", "Buy");
+        }
+
+        private static async Task SendMail(Members currentuser, List<Models.Product_Detail.ProductBasketModels> basket, Orders order)
+        {
+            string templatePath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "EmailTemplate/template.html");
+            string html = System.IO.File.ReadAllText(templatePath);
+            html = html.Replace("&&Ad&&", currentuser.Name);
+            html = html.Replace("&&Soyad&&", currentuser.Surname);
+            html = html.Replace("&&Eamil&&", currentuser.Email);
+            html = html.Replace("&&Tel&&", currentuser.PhoneNumber);
+            html = html.Replace("&&Adres&&", order.Address);
+            string products = "";
+            foreach (Models.Product_Detail.ProductBasketModels item in basket)
+            {
+                products += $@"<tr> <td>
+                                                                            Ürün Adı:
+                                                                        </td>
+                                                                        <td>{item.Products.Name}</td>
+                                                                    </tr>
+                                                                    <tr>
+
+                                                                        <td>
+                                                                            Ürün Fiyatı:
+                                                                        </td>
+                                                                        <td>{item.Products.Price}</td>
+                                                                    </tr>
+                                                                    <tr>
+
+                                                                        <td>
+                                                                            Ürün Adet Bilgisi:
+                                                                        </td>
+                                                                        <td>{item.Count}</td>
+                                                                    </tr>";
+            }
+            html = html.Replace("&&Urun&&", products);
+            var emailModel = new EmailModel()
+            {
+                FromEmailAdress = "danismaz2000@gmail.com",
+                ToEmailAdress = "danismaz2000@gmail.com",
+                HtmlContent = html,
+                Subject = "Yeni bir spariş alındı."
+            };
+            await EmailSender.SendMail(emailModel);
+        }
+
+        [HttpGet]
+        public ActionResult Sparişlerim(int? page)
+        {
+            var currentuser = (UsKirtasiye.DB.Members)Session["LogonUser"];
+            if (Session["LogonUser"] == null)
+            {
+                return RedirectToAction("Index", "Index");
+            }
+            else
+            {
+                var orders = context.OrderDetails
+                    .Include("Orders")
+                    .Include("Products")
+                    .Where(x => x.Orders.Member_Id == currentuser.Id)
+                    .Select(x => new MemberOrderDetail
+                    {
+                        Productid = x.Product_Id,
+                        ProductName = x.Products.Name,
+                        ProductPhoto = x.Products.Photo,
+                        ProductPrice = x.Products.Price,
+                        Quantity = x.Quantity,
+                    }).ToList();
+
+                //var orders = context.Orders.Where(x => x.Member_Id == currentuser.Id);
+                //List<Models.OrderModels> model = new List<OrderModels>();
+                //foreach (var item in orders)
+                //{
+                //    var bymodel = new OrderModels();
+                //    bymodel.TotalPrice = item.OrderDetails.Sum(y => y.Price);
+                //    bymodel.OrderName = string.Join("////", item.OrderDetails.Select(y => y.Products.Name + " (" + y.Quantity + "Adet" + ")"));
+                //    model.Add(bymodel);
+                //}
+
+                List<string> asd = new List<string>();
+
+                int pageSize = 10;
+                int pageNumber = (page ?? 1);
+                return View(orders.ToPagedList(pageNumber, pageSize));
+            }
+        }
+    }
+}
---
+++ UsKirtasiye/Controllers/ProfileController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsKirtasiye.DB;
+using UsKirtasiye.Models;
+
+namespace UsKirtasiye.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private UsKirtasiyeEntities context;
+
+        public ProfileController()
+        {
+            context = new UsKirtasiyeEntities();
+        }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            if (Session["LogonUser"] == null)
+            {
+                return RedirectToAction("Index", "Index");
+            }
+            var currentUser = (DB.Members)Session["LogonUser"];
+            DB.Members user = context.Members.FirstOrDefault(x => x.Id == currentUser.Id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult ProfileEdit(DB.Members üyekayıt)
+        {
+            if (üyekayıt.Password.Length < 6)
+            {
+                TempData["YenilemeHatası"] = "X Şifre 6 karakterden az olamaz";
+            }
+            else if (üyekayıt.PhoneNumber.Length != 10)
+            {
+                TempData["YenilemeHatası"] = "X  Telefon numarası 10 karakterden oluşmalıdır. ";
+            }
+            else
+            {
+                üyekayıt.ModifiedDate = DateTime.Now;
+                context.Entry<DB.Members>(üyekayıt).State = System.Data.Entity.EntityState.Modified;
+                TempData["Doğrulama"] = "✓  Kullanıcı başarı ile güncellenmiştir";
+                context.SaveChanges();
+            }
+            return RedirectToAction("Profile", "Profile");
+        }
+
+        [HttpPost]
+        public ActionResult AdminProfile(DB.Members üyekayıt)
+        {
+            if (üyekayıt.Password.Length < 6)
+            {
+                TempData["YenilemeHatası"] = "X Şifre 6 karakterden az olamaz";
+            }
+            else if (üyekayıt.PhoneNumber.Length != 10)
+            {
+                TempData["YenilemeHatası"] = "X  Telefon numarası 10 karakterden oluşmalıdır. ";
+            }
+            else
+            {
+                context.Entry<DB.Members>(üyekayıt).State = System.Data.Entity.EntityState.Modified;
+                TempData["Doğrulama"] = "✓  Kullanıcı başarı ile güncellenmiştir";
+                context.SaveChanges();
+            }
+            return RedirectToAction("AdminProfile", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult Adresler()
+        {
+            if (Session["LogonUser"] == null)
+            {
+                return RedirectToAction("Index", "Index");
+            }
+            var currentUser = (DB.Members)Session["LogonUser"];
+
+            var addresses = context.Addresses.Where(x => x.Member_Id == currentUser.Id && x.IsActive == true).ToList();
+            if (addresses.Any())
+            {
+                return View(addresses);
+            }
+            else
+            {
+                return RedirectToAction("AdresEkle", "Profile");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Adresler(DB.Addresses adres)
+        {
+            var currentUser = (DB.Members)Session["LogonUser"];
+            var dbadress = context.Addresses.FirstOrDefault(x => x.Id == adres.Id);
+            dbadress.Member_Id = currentUser.Id;
+            dbadress.Name = adres.Name;
+            dbadress.AdresDescription = adres.AdresDescription;
+            dbadress.ModifiedDate = DateTime.Now;
+            TempData["AdresDoğrulama"] = "✓  Adres Başarı ile güncellenmiştir.";
+            context.SaveChanges();
+            return RedirectToAction("Adresler", "Profile");
+        }
+
+        [HttpGet]
+        public ActionResult AdresEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdresEkle(DB.Addresses adres)
+        {
+            DB.Addresses adresler = context.Addresses.FirstOrDefault(x => x.AdresDescription == adres.AdresDescription);
+            if (adresler != null)
+            {
+                TempData["AdresHata"] = "Böyle bir adres zaten kayıtlıdır.";
+            }
+            else
+            {
+                var currentuser = (UsKirtasiye.DB.Members)Session["LogonUser"];
+                adres.AddedDate = DateTime.Now;
+                adres.Member_Id = currentuser.Id;
+                adres.IsActive = true;
+                context.Addresses.Add(adres);
+                context.SaveChanges();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DeleteAdress(int adressId)
+        {
+            var dbadress = context.Addresses.FirstOrDefault(x => x.Id == adressId);
+            dbadress.IsActive = false;
+            dbadress.ModifiedDate = DateTime.Now;
+            context.SaveChanges();
+            return RedirectToAction("Adresler", "Profile");
+        }
+    }
+}
